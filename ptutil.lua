2bee7937114fe148609ce15ea2ec1d458f55a5bd
Joshua Cantara <1975723+joshuacant@users.noreply.github.com>
2025-11-21 20:24:03 -0500

    fix sans font declarations

--- ptutil.lua
+++ ptutil.lua
@@ -107,9 +107,9 @@ ptutil.good_serif_it = "source/SourceSerif4-It.ttf"
 ptutil.good_serif_bold = "source/SourceSerif4-Bold.ttf"
 ptutil.good_serif_boldit = "source/SourceSerif4-BoldIt.ttf"
 ptutil.good_sans = "source/SourceSans3-Regular.ttf"
-ptutil.good_sans_it = "source/SourceSans4-It.ttf"
-ptutil.good_sans_bold = "source/SourceSans4-Bold.ttf"
-ptutil.good_sans_boldit = "source/SourceSans4-BoldIt.ttf"
+ptutil.good_sans_it = "source/SourceSans3-It.ttf"
+ptutil.good_sans_bold = "source/SourceSans3-Bold.ttf"
+ptutil.good_sans_boldit = "source/SourceSans3-BoldIt.ttf"
 ptutil.title_serif = ptutil.good_serif_boldit
 
 -- a non-standard space is used here because it looks nicer
